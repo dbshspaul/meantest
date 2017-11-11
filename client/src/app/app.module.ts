149895f7d43c3cd478cfd89f14644ff178c11429
dbshspaul <dbshspaul@gmail.com>
2017-11-11 12:27:46 +0530

    New changelist

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,23 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ContactComponent} from './contact/contact.component';
-import {RouterModule, Routes} from "@angular/router";
-import {contactRoutes} from "./contact/contact.route";
-
-const ENTITY_STATES = [
-  ...contactRoutes
-];
+import {RouterModule} from "@angular/router";
+import {ContactModule} from "./contact/contact.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactComponent
+    AppComponent
   ],
-  imports: [RouterModule.forRoot(
-    ENTITY_STATES,
-    {enableTracing: true, useHash: true}
-  ),
+  imports: [
+    RouterModule,
+    ContactModule,
     BrowserModule
   ],
   providers: [],
